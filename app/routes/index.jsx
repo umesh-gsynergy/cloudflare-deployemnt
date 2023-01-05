482147279a42dbc66855620c6d225a05ce9669c2
Umesh Yadav <umeshyadav@Umeshs-MacBook-Air.local>
2023-01-05 14:38:39 +0530

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <main>
+        <p>This is a demo</p>
+      </main>
+    </div>
+  );
+}
